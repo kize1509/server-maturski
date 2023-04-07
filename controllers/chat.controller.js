ab4ab1088199f8a47b601538af6e57804ab76209
kize1509 <130162294+kize1509@users.noreply.github.com>
2023-04-07 23:02:02 +0200

    structure change1

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,111 +1,5 @@
-const { response } = require("express");
 const db = require("../controllers/db.controller");
 
-function getUserByUsernameAndPassword(req, res){
-    const user = req.params.username;
-    const pass = req.params.password;
-
-
-    db.query(`SELECT * FROM maturski.users WHERE username='${user}' AND password='${pass}';`, function(err, result){
-        if(err){
-            console.log(err);
-            res.send("error");
-        }else{
-            res.send(result);
-        }
-    });
-}
-
-
-function saveNewUser(req, res){
-
-    const afirmativeResponse = {
-        message: "Successfully appended the database!"
-    }
-
-    const rejectionResponse = {
-        message: "Unable to save the user!"
-    }
-    
-    const invalidCredentials = {
-        message: "Uslovi nisu ispunjeni!"
-    }
-
-    let username = req.body.username;
-    let password = req.body.password;
-    let razredOdeljenje = req.body.razred;
-
-    let mailFormat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let passFormat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,20}$/;
-    let razredFormat = /^(I(V|I{0,2})-([1-9]|11|10))$|^((V\I{2,3})\-[1-2])|adm|prof$/;
-
-    if(username.toLowerCase().match(mailFormat) && razredOdeljenje.match(razredFormat) && password.match(passFormat)){
-
-        db.query(`SELECT * FROM users WHERE username='${username}'`, function(err, result){
-            if(err){
-                
-                res.send(err);
-            }else{
-                if(result.length>0){
-                    let response = JSON.stringify(rejectionResponse);
-                    res.send(response);
-                }else{
-                    
-                    db.query(`INSERT INTO users (username, password, razred) VALUES ('${username}', '${password}', '${razredOdeljenje}')`, function(err, result){
-                            if (err) {
-                                let response = JSON.stringify(rejectionResponse);
-                                res.send(response);
-                                console.log(err);
-                            }else{
-                                let response = JSON.stringify(afirmativeResponse);
-                                res.send(response);
-                            }
-                });
-                }
-            }
-        });
-    }else{
-
-        res.send(invalidCredentials);
-    }
-
-}
-
-function getAllUsers(req, res){
-
-    const errorJson = {message:"error while reading the database"}
-    let username = req.params.username;
-
-    db.query(`SELECT * FROM users WHERE username != '${username}'`, function (err, result) {
-       if(err){
-        let errorMessage = JSON.stringify(errorJson);
-        res.send(errorMessage);
-       } else {
-            res.send(result);
-       }
-    });
-}
-
-function deleteUser(req, res) {
-    const errorMessage = {
-        message: "UNABLE TO DELETE THE USER!"
-    }    
-
-    const successMessage = {
-        message: "SUCCESS!"
-    }
-
-    let username = req.params.username;
-
-    db.query(`DELETE FROM users WHERE username = '${username}'`, function(err, result){
-        if(err){
-            res.send(JSON.stringify(errorMessage));
-        }
-        if(result){
-            res.send(JSON.stringify(successMessage));
-        }
-    })
-}
 
 function getMessages(req, res){
     const errorMessage = {
@@ -200,4 +94,5 @@ function getAllRooms(req, res){
     
 }
 
-module.exports = {getUserByUsernameAndPassword, saveNewUser, getAllUsers, deleteUser, getMessages, createANewChat, getAllRooms}
+
+module.exports = {getMessages, createANewChat, getAllRooms};
--- data.router.js
+++ data.router.js
@@ -1,16 +1,17 @@
 const express = require("express");
 
-const dataController = require("./controllers/data.controller");
+const userController = require("./controllers/user.controller.js");
+const chatController = require("./controllers/chat.controller.js");
 
 const dataRouter = express.Router();
 
 
-dataRouter.get("/messages/:room", dataController.getMessages);
-dataRouter.get("/:username&:password", dataController.getUserByUsernameAndPassword);
-dataRouter.get("/:username", dataController.getAllUsers);
-dataRouter.post("/new", dataController.saveNewUser);
-dataRouter.delete("/del:username", dataController.deleteUser);
-dataRouter.post("/newChat", dataController.createANewChat);
-dataRouter.get("/allRooms/:username", dataController.getAllRooms);
+dataRouter.get("/messages/:room", chatController.getMessages);
+dataRouter.get("/:username&:password", userController.getUserByUsernameAndPassword);
+dataRouter.get("/:username", userController.getAllUsers);
+dataRouter.post("/new", userController.saveNewUser);
+dataRouter.delete("/del:username", userController.deleteUser);
+dataRouter.post("/newChat", chatController.createANewChat);
+dataRouter.get("/allRooms/:username", chatController.getAllRooms);
 
 module.exports = { dataRouter };
